1a9773b40600c39c7437c293b4cdc36d0fea037c
luorenjie <lrj193927@gmail.com>
2020-11-13 17:21:46 -0500

    add dependency injections

---
+++ src/main/java/org/northernjay/sfgdi/SfgDiApplication.java
@@ -0,0 +1,41 @@
+package org.northernjay.sfgdi;
+
+import org.northernjay.sfgdi.controllers.ConstructorInjectedController;
+import org.northernjay.sfgdi.controllers.MyController;
+import org.northernjay.sfgdi.controllers.PropertyInjectedController;
+import org.northernjay.sfgdi.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("---------- Property");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+
+		System.out.println("---------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("---------- Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
+
+	}
+
+}
